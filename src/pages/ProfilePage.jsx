d59bf12b25706821b35d1a30db898868aa897133
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-05 20:33:19 +0530

    Add ProfilePage component with Navbar and Footer integration

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+const ProfilePage = () => {
+  return (
+    <>
+      <Navbar/>
+      <main>
+        <p>profile page</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export { ProfilePage};
