d8be5f350162052ca23d0062004f50311a14eb45
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-05 20:32:10 +0530

    Add Navbar component with search functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router";
+const Navbar = () => {
+  const [searchParams,setSearchParams]=useSearchParams();
+  const [searchText,setSearchText]=useState(searchParams.get("text")|| "");
+  //const [searchText, setSearchText] = useState();
+  let navigate = useNavigate();
+  const handleSearch = () => {
+    navigate(`/search?text=${searchText}`);
+  };
+  return (
+    <nav className="flex px-6 py-4 justify-between items-center bg-emerald-200">
+      <p>My App</p>
+      <div>
+        <input
+          className="px-2 py-1 border-1 border-amber-800 rounded-lg mr-2"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          onClick={handleSearch}
+          className="px-2 py-1 border-1 border-amber-800 rounded-lg bg-amber-700 text-white cursor-pointer"
+        >
+          search
+        </button>
+      </div>
+      <div className="h-6 w-6 bg-blue-600 rounded-full"></div>
+    </nav>
+  );
+};
+
+export { Navbar };
