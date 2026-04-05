f3af26ffbb6ecb7178341324fd821a428677521d
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-05 20:31:47 +0530

    Add Footer component with copyright notice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='px-6 py-4 bg-cyan-200 text-center'>
+            <p>&copy; All rights reserved to myapp.com</p>
+        </footer>
+    </div>
+  )
+}
+
+export {Footer};
