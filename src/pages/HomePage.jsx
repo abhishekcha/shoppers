808b3cab679b114a28648965b18f71bc11eb63ab
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-05 20:33:06 +0530

    Add HomePage component with Navbar and Footer integration

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+const HomePage = () => {
+  return (
+    <>
+      <Navbar/>
+      <main>
+        <p>HomePage</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export { HomePage };
